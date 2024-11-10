4119bffd46e58d6f0bf0404da68d26a616260008
Jess Hatfield <joshhatfield@qogita.com>
2024-11-10 11:50:11 +0000

    chore: added zero-shot classifier

--- src/classifiers/base_classifier.py
+++ src/classifiers/base_classifier.py
@@ -1,7 +1,11 @@
 from abc import ABC
 
+from werkzeug.datastructures import FileStorage
+
+from src.enums.document_types import DocumentType
+
 
 class DocumentClassifier(ABC):
 
-    def classify(self):
+    def classify(self, file: FileStorage) -> DocumentType:
         raise NotImplementedError
