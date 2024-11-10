7998a60358e95084b1ba582033e67d0059407520
Jess Hatfield <joshhatfield@qogita.com>
2024-11-10 13:19:14 +0000

    chore: refactored classifier class

--- src/classifier.py
+++ src/classifier.py
@@ -1,21 +1,34 @@
+import dataclasses
+
 from werkzeug.datastructures import FileStorage
 
+from src.classifiers.base_classifier import ClassifierResult
 from src.classifiers.finance_industry_document_classifier import FinancialDocumentClassifier
-from src.enums.document_types import DocumentType
+from src.enums.file_types import GenericDocumentTypes
+
+
+@dataclasses.dataclass
+class ClassificationResults:
+    results: [ClassifierResult]
+
+    def get_document_label(self):
+
+        highest_score = 0
+        label = GenericDocumentTypes.UNKNOWN_DOCUMENT_TYPE
+
+        for result in self.results:
+            if result.score > highest_score:
+                highest_score = result.score
+                label = result.label
+
+        return label
 
 
 def classify_file(file: FileStorage):
     classifiers = [FinancialDocumentClassifier()]
-
-    # build a list of industry classifiers
-    # store results
-    # return result with highest confidence rating?
+    results = ClassificationResults()
 
     for classifier in classifiers:
-        document_type = classifier.classify(file=file)
+        results.append(classifier.classify(file=file))
 
-    if document_type:
-        return document_type
-
-    # If we can't retrieve any classifications return hardcoded value
-    return DocumentType.UNKNOWN_FILE.value
+    return results.get_document_label()
--- src/enums/file_types.py
+++ src/enums/file_types.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
-
+class GenericDocumentTypes(Enum):
+    UNKNOWN_DOCUMENT_TYPE='unknown_document_type'
 
 class SupportedFileTypes(Enum):
     PDF = 'application/pdf'
