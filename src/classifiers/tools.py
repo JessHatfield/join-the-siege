f2023e779cad72730af7db5a93c8cfe67055a51b
Jess Hatfield <joshhatfield@qogita.com>
2024-11-11 10:50:53 +0000

    chore:  forces huggingface to use local model

--- src/classifiers/tools.py
+++ src/classifiers/tools.py
@@ -8,7 +8,7 @@ logger = structlog.getLogger(__name__)
 def deberta_v3_classifier(extracted_text: str, candidate_labels: [str], industry: str) -> ClassifierResult:
     model_name = "MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
     classifier = pipeline(model=model_name)
-    result = classifier(extracted_text, candidate_labels, multi_label=False)
+    result = classifier(extracted_text, candidate_labels, multi_label=False, local_files_only=True)
     # Classifier always returns the most likely label in position 0!
     logger.debug('classification_generated', model=model_name, results=result)
 
